3d7912aa7576676fd86254b84d734348c453e335
Vladick <vlad2004ki@gmail.com>
2024-03-18 14:51:45 +0300

    очень люблю писать summary, ведь с ними 10 строчек превращаются в 40 =)

--- Figures/ArrayPoints.cs
+++ Figures/ArrayPoints.cs
@@ -12,6 +12,11 @@ public class ArrayPoints
     private int index = 0;
     private Point[] points;
 
+    /// <summary>
+    /// Конструктор класса ArrayPoints, который инициализирует массив точек размером size. 
+    /// Если size меньше или равен 0, то размер массива устанавливается равным 2.
+    /// </summary>
+    /// <param name="size">Изначальный размер точки</param>
     public ArrayPoints(int size)
     {
         if (size <= 0) size = 2;
@@ -19,6 +24,13 @@ public class ArrayPoints
         points = new Point[size];
     }
 
+    /// <summary>
+    /// Метод, который устанавливает точку с координатами (x, y) в массив точек. 
+    /// Если индекс достигает размера массива, он сбрасывается до 0. 
+    /// Индекс увеличивается на 1 после установки точки.
+    /// </summary>
+    /// <param name="x">точка координат</param>
+    /// <param name="y">точка координат</param>
     public void SetPoint(int x, int y)
     {
         if (index >= points.Length) index = 0;
@@ -27,9 +39,20 @@ public class ArrayPoints
         index++;
     }
 
+    /// <summary>
+    /// Метод, который сбрасывает индекс до 0, возвращая массив точек к его начальному состоянию.
+    /// </summary>
     public void ResetPoints() { index = 0; }
 
+    /// <summary>
+    /// Метод, который возвращает текущее количество точек в массиве.
+    /// </summary>
+    /// <returns></returns>
     public int GetPointsCount() => index;
 
+    /// <summary>
+    /// Метод, который возвращает массив точек.
+    /// </summary>
+    /// <returns></returns>
     public Point[] GetPoints() => points;
 }
--- Paint5D/Form1.cs
+++ Paint5D/Form1.cs
@@ -7,11 +7,11 @@ public partial class Form1 : Form
 {
     private bool isMouse = false;
 
-    private ArrayPoints arrayPoints = new(2);
+    private readonly ArrayPoints arrayPoints = new(2);
     private Bitmap map = new(100, 100);
     private Graphics graphics;
-    private Pen pen = new(Color.Black, 3f);
-    private Pen erase = new(Color.White, 6f);
+    private readonly Pen pen = new(Color.Black, 3f);
+    private readonly Pen erase = new(Color.White, 12f);
     private int choosedTool = 1;
 
     private void SetBitMapSize()
@@ -30,10 +30,7 @@ public partial class Form1 : Form
     }
 
 
-    private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
-    {
-        isMouse = true;
-    }
+    private void pictureBox1_MouseDown(object sender, MouseEventArgs e) { isMouse = true; }
 
     private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
     {
@@ -48,20 +45,16 @@ public partial class Form1 : Form
         arrayPoints.SetPoint(e.X, e.Y);
 
         if (arrayPoints.GetPointsCount() >= 2)
-        {
             if (choosedTool == 1 || choosedTool == 2)
             {
                 graphics.DrawLines(choosedTool == 1 ? pen : erase, arrayPoints.GetPoints());
                 pictureBox1.Image = map;
                 arrayPoints.SetPoint(e.X, e.Y);
             }
-        }
     }
 
-    private void button3_Click(object sender, EventArgs e) //универсальная кнопка цветовой палитры
-    {
-        pen.Color = ((Button)sender).BackColor;
-    }
+    //универсальная кнопка цветовой палитры
+    private void button3_Click(object sender, EventArgs e) { pen.Color = ((Button)sender).BackColor; }
 
     private void button5_Click(object sender, EventArgs e) //color dialog
     {
@@ -78,33 +71,20 @@ public partial class Form1 : Form
         pictureBox1.Image = map;
     }
 
-    private void trackBar1_ValueChanged(object sender, EventArgs e)
-    {
-        pen.Width = trackBar1.Value;
-    }
+    private void trackBar1_ValueChanged(object sender, EventArgs e) { pen.Width = trackBar1.Value; }
 
     private void toolStripButton2_Click(object sender, EventArgs e)
     {
         saveFileDialog1.Filter = "JPG(*.JPG)|*.jpg";
 
         if (saveFileDialog1.ShowDialog() == DialogResult.OK)
-        {
             if (pictureBox1 != null)
-            {
                 pictureBox1.Image.Save(saveFileDialog1.FileName);
-            }
-        }
     }
 
-    private void button11_Click(object sender, EventArgs e)
-    {
-        choosedTool = 2;
-    }
+    private void button11_Click(object sender, EventArgs e) { choosedTool = 2; }
 
-    private void button12_Click(object sender, EventArgs e)
-    {
-        choosedTool = 1;
-    }
+    private void button12_Click(object sender, EventArgs e) { choosedTool = 1; }
 
     private void label2_Click(object sender, EventArgs e)
     {
@@ -113,6 +93,6 @@ public partial class Form1 : Form
 
     private void toolStripButton3_Click(object sender, EventArgs e)
     {
-
+        //метод удаления последней добавленной линии
     }
 }
